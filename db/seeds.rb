1db2436ea1068c856ba075e6f263bdf1143b3d89
Mark Wanjau <theafrikanson@gmail.com>
2023-06-06 14:13:21 +0300

    seeds check

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding data..."
 
-# Seed your database here
+# run a loop 50 times
+
+50.times do
+    # create a game with random data
+    product = Product.create(
+      name: Faker::Commerce.product_name,
+      price: rand(0..100) # random number between 0 and 100
+    )
+
+    user = User.create(
+      name: Faker::Name.name
+    )
+  
+    # # create between 0 and 10 reviews for each product
+    rand(0..10).times do
+      Review.create(
+        product_id: product.id,
+        user_id: user.id,
+        star_rating: rand(0..10),
+        comment: Faker::Lorem.sentence     
+      )
+    end
+  end
 
 puts "✅ Done seeding!"
