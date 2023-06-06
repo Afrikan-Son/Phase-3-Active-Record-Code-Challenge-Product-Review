622da5ffa8fc403f3c72a96b7430934eae54fe8f
Mark Wanjau <theafrikanson@gmail.com>
2023-06-06 13:21:58 +0300

    migr. product

--- db/migrate/20230606100208_create_products.rb
+++ db/migrate/20230606100208_create_products.rb
@@ -1,4 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
+
+    create_table :products do |t|
+      t.string :name, null: false
+      t.integer :price, default: 0, null: false
+    end
+    
   end
 end
